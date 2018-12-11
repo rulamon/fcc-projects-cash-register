0486ebb837bb1f3983b52bca9ed0bc6e46f150af
rulamon <ruben.lamon@gmail.com>
2018-12-11 22:03:55 +0100

    first solution, had to patch up using Math.round()

--- 1.js
+++ 1.js
@@ -1,20 +1,31 @@
 function checkCashRegister(price, cash, cid) {
-	const changeFunc = (num, arr) => {
-		
-	}
-	let change = cash - price;
-	let totalInDrawer = cid.map(val => val[1]).reduce((acc, val) => (acc * 100 + val * 100) / 100);
+	const currencyArr = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
 	let result = {status: "", change: []}
-	if (change > totalInDrawer) {
-		result.status = "INSUFFICIENT_FUNDS"
-	} else if (change === totalInDrawer) {
-		result.status = "CLOSED";
+	let change = (cash * 100 - price * 100) / 100 ;
+	let totalInDrawer = cid.map(val => val[1]).reduce((acc, val) => (acc * 100 + val * 100) / 100);
+	if (change === totalInDrawer) {
+		result.status = "CLOSED"
 		result.change = cid;
-	} else if (change < totalInDrawer) {
-		// function
+	} else {
+		for (let i = cid.length - 1; i >= 0 && change > 0; i--) {
+			let currInReg = cid[i][1];
+			let curr = currencyArr[i];
+			if (change > curr && currInReg > 0 && change >= currInReg) {
+				result.change.push(cid[i]);
+				change = (change * 100 - currInReg * 100) / 100;
+			} else if (change > curr && currInReg > 0 && change < currInReg) {
+				result.change.push([cid[i][0], (change * 100 - (change * 100) % (curr * 100))/100]);
+				change = (Math.round(change * 100) % (curr * 100)) / 100;
+			}
+		}
+		if (change === 0 && totalInDrawer > 0) {
+			result.status = "OPEN"
+		} else if (change > 0) {
+			result.status = "INSUFFICIENT_FUNDS"
+			result.change = [];
+		}
 	}
-	return result
-
+	return result;
 }
 
 // Example cash-in-drawer array:
@@ -28,5 +39,4 @@ function checkCashRegister(price, cash, cid) {
 // ["TWENTY", 60],
 // ["ONE HUNDRED", 100]]
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], 
-	["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
